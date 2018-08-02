aa39eee8a6703c428cdbe2b52256e885db13d31f
JocelynTse <jocelyn.s.tse@gmail.com>
2018-08-02 00:22:54 -0500

    data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,98 +1,100 @@
-[
+let friendsData = [
     {
-        "name": "Joy",
-        "photo": "https://vignette.wikia.nocookie.net/pixar/images/7/75/Io_Joy_standard2.jpg/revision/latest?cb=20150425021126",
-        "scores": [
-            "4",
-            "1",
-            "4",
-            "4",
-            "4",
-            "1",
-            "2",
-            "4",
-            "4",
-            "1"
+        name: "Joy",
+        photo: "https://vignette.wikia.nocookie.net/pixar/images/7/75/Io_Joy_standard2.jpg/revision/latest?cb=20150425021126",
+        scores: [
+            4,
+            1,
+            4,
+            4,
+            4,
+            1,
+            2,
+            4,
+            4,
+            1
         ]
     },
     {
-        "name": "Sadness",
-        "photo": "https://vignette.wikia.nocookie.net/pixar/images/0/06/Io_Sadness_standard2.jpg/revision/latest?cb=20150425021239",
-        "scores": [
-            "4",
-            "2",
-            "4",
-            "1",
-            "3",
-            "2",
-            "2",
-            "1",
-            "3",
-            "2"
+        name: "Sadness",
+        photo: "https://vignette.wikia.nocookie.net/pixar/images/0/06/Io_Sadness_standard2.jpg/revision/latest?cb=20150425021239",
+        scores: [
+            4,
+            2,
+            4,
+            1,
+            3,
+            2,
+            2,
+            1,
+            3,
+            2
         ]
     },
     {
-        "name": "Anger",
-        "photo": "https://vignette.wikia.nocookie.net/pixar/images/7/7a/Io_Anger_standard2.jpg/revision/latest?cb=20150425021210",
-        "scores": [
-            "4",
-            "2",
-            "2",
-            "2",
-            "4",
-            "1",
-            "3",
-            "2",
-            "4",
-            "4"
+        name: "Anger",
+        photo: "https://vignette.wikia.nocookie.net/pixar/images/7/7a/Io_Anger_standard2.jpg/revision/latest?cb=20150425021210",
+        scores: [
+            4,
+            2,
+            2,
+            2,
+            4,
+            1,
+            3,
+            2,
+            4,
+            4
         ]
     },
     {
-        "name": "Disgust",
-        "photo": "https://vignette.wikia.nocookie.net/pixar/images/9/98/Io_Disgust_standard2.jpg/revision/latest?cb=20170829015054",
-        "scores": [
-            "3",
-            "3",
-            "4",
-            "2",
-            "2",
-            "1",
-            "3",
-            "2",
-            "2",
-            "3"
+        name: "Disgust",
+        photo: "https://vignette.wikia.nocookie.net/pixar/images/9/98/Io_Disgust_standard2.jpg/revision/latest?cb=20170829015054",
+        scores: [
+            3,
+            3,
+            4,
+            2,
+            2,
+            1,
+            3,
+            2,
+            2,
+            3
         ]
     },
     {
-        "name": "Fear",
-        "photo": "https://vignette.wikia.nocookie.net/pixar/images/7/79/Io_Fear_standard2.jpg/revision/latest?cb=20150425021148",
-        "scores": [
-            "4",
-            "3",
-            "4",
-            "1",
-            "4",
-            "2",
-            "4",
-            "3",
-            "1",
-            "4"
+        name: "Fear",
+        photo: "https://vignette.wikia.nocookie.net/pixar/images/7/79/Io_Fear_standard2.jpg/revision/latest?cb=20150425021148",
+        scores: [
+            4,
+            3,
+            4,
+            1,
+            4,
+            2,
+            4,
+            3,
+            1,
+            4
         ]
     },
     {
-        "name": "BingBong",
-        "photo": "https://vignette.wikia.nocookie.net/pixar/images/8/85/1923_BingBong_InsideOut_501_copie.jpg/revision/latest?cb=20150611010239",
-        "scores": [
-            "4",
-            "4",
-            "2",
-            "3",
-            "2",
-            "2",
-            "3",
-            "2",
-            "4",
-            "4"
+        name: "BingBong",
+        photo: "https://vignette.wikia.nocookie.net/pixar/images/8/85/1923_BingBong_InsideOut_501_copie.jpg/revision/latest?cb=20150611010239",
+        scores: [
+            4,
+            4,
+            2,
+            3,
+            2,
+            2,
+            3,
+            2,
+            4,
+            4
         ]
     }
-]
+];
+
+module.exports = friendsData;
